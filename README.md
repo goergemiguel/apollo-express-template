d4c6675c9f815de13f108afc3dfc9351f66c8e0e
goergemiguel <goergemiguel21@gmail.com>
2021-07-17 15:27:03 +0800

    add env vars

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # apollo-express-template
+
+### Environment variables
+API_PORT - specify which port to run on
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { config as getEnvironmentVars } from "dotenv"
 import express from "express"
 import { ApolloServer } from "apollo-server-express"
 
@@ -5,14 +6,15 @@ import typeDefs from "./typeDefs/index.js"
 import resolvers from "./resolvers/index.js"
 
 async function startApolloServer() {
+	getEnvironmentVars()
 	const server = new ApolloServer({ typeDefs, resolvers })
 	await server.start()
 
 	const app = express()
 	server.applyMiddleware({ app })
 
-	await new Promise((resolve) => app.listen({ port: 4000 }, resolve))
-	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+	await new Promise((resolve) => app.listen({ port: process.env.API_PORT }, resolve))
+	console.log(`🚀 Server ready at http://localhost:${process.env.API_PORT}${server.graphqlPath}`)
 	return { server, app }
 }
 
